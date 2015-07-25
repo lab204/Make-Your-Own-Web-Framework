559aee35485858dcd861c2ca31fbec81966929cf
sllt <shenlongml@gmail.com>
2015-07-25 20:06:36 +0800

    update

--- hamster/lib/hamster.rb
+++ hamster/lib/hamster.rb
@@ -1,9 +1,15 @@
+#coding: utf-8
 require_relative "hamster/version"
 require_relative "hamster/routing"
 
 module Hamster
   class Application
     def call(env)
+      if env["PATH_INFO"] == "/favicon.ico"
+        return [404, {'Content-Type' => 'text/html'},
+          []]
+      end
+      
       controller, action  = get_controller_and_action(env)
       content = controller.new(env).send(action)
       [200, {'Content-Type' => 'text/html'},
